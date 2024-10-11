1f5bc123917b914862e1abdfadc934713128f8df
Ingrid <ingrid10beth@gmail.com>
2024-10-11 12:30:59 -0600

    Subiendo proyecto Android y documento Word

---
+++ app/src/main/java/com/example/decisionestarea/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.decisionestarea;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText etR;
+    private EditText etA;
+    private EditText etRE;
+    private TextView txtResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        etR = findViewById(R.id.etR);
+        etA = findViewById(R.id.etA);
+        etRE = findViewById(R.id.etRE);
+        txtResult = findViewById(R.id.txtResult);
+        Button btnVerificar = findViewById(R.id.btnV);
+
+        btnVerificar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                verificar(); 
+            }
+        });
+    }
+
+    public void verificar() {
+        //para que se conviertan a minusculas
+        String recubrimiento = etR.getText().toString().toLowerCase();
+        String alimentacion = etA.getText().toString().toLowerCase();
+        String reproduccion = etRE.getText().toString().toLowerCase();
+
+        String resultado = "";
+
+        // tipo de animal
+        if (recubrimiento.equals("pelo") && alimentacion.equals("carne") && reproduccion.equals("vientre")) {
+            resultado = "Es un mamífero";
+        } else if (recubrimiento.equals("plumas") && alimentacion.equals("semillas") && reproduccion.equals("huevos")) {
+            resultado = "Es un ave";
+        } else if (recubrimiento.equals("escamas") && alimentacion.equals("insectos") && reproduccion.equals("huevos")) {
+            resultado = "Es un reptil";
+        } else {
+            resultado = "No se pudo determinar el tipo de animal";
+        }
+
+
+        txtResult.setText(resultado);
+    }
+
+
+}
